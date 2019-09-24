d98393f974869f8a5b261260782be76be54467ee
Richard McSharry <rmcsharry@gmail.com>
2019-09-24 21:51:41 +0200

    Adds cache interceptor for people route

---
+++ src/app/interceptors/cache.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+  HttpResponse
+} from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { tap } from "rxjs/operators";
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+  private cache = new Map<string, any>();
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!request.url.includes('people')) {
+      return next.handle(request);
+    };
+    if (request.method !== "GET") {
+      return next.handle(request);
+    };
+    console.warn("CacheInterceptor");
+    const cachedResponse = this.cache.get(request.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    };
+
+    return next.handle(request).pipe(
+      tap(response => {
+        if (response instanceof HttpResponse) {
+          this.cache.set(request.url, response);
+        };
+      })
+    );
+  }
+}
--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -2,9 +2,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import {ApiBaseInterceptor} from './api-base.interceptor';
 import {HttpErrorInterceptor} from './http-error.interceptor';
+import { CacheInterceptor } from './cache.interceptor';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: ApiBaseInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
-
 ];
